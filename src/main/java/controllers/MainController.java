14ff2978295287d81267e50f9f8ccc8a170680e9
katandser <katandser96@mail.ru>
2019-06-01 15:30:51 +0700

    заглушка

--- src/main/java/controllers/MainController.java
+++ src/main/java/controllers/MainController.java
@@ -1,11 +1,21 @@
 package controllers;
 
+import jdbc.ConnectToDB;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
 @Controller
 public class MainController {
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        List<String> list = ConnectToDB.getSt();
+        model.addAttribute("object",list);
+        for (String s: list) {
+            System.out.println(s);
+        }
         return "index";
     }
 }
--- src/main/java/jdbc/ConnectToDB.java
+++ src/main/java/jdbc/ConnectToDB.java
@@ -1,4 +1,57 @@
 package jdbc;
 
-public class ConnectToDB {
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConnectToDB implements AutoCloseable {
+    private static final String DB_URL = "jdbc:clickhouse://localhost:8123/db_test";
+    private final Connection conn;
+
+    /**
+     * Creates new instance
+     * @throws SQLException in case of connection issue
+     */
+    public ConnectToDB() throws SQLException {
+        conn = DriverManager.getConnection(DB_URL,"","1");
+    }
+
+    /**
+     * Queries db to get most popular flight route for ths given year
+     * @throws SQLException in case of query issue
+     */
+    public void getShops() throws SQLException {
+        String query = "select * from db_test.events where site_id = 1";
+        Statement statement = conn.createStatement();
+        ResultSet rs = statement.executeQuery(query);
+        ResultSetMetaData rsMetadata = rs.getMetaData();
+        while (rs.next()) {
+            for (int i = 1; i <= rsMetadata.getColumnCount(); i++) {
+                System.out.print(rs.getString(i) + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static List<String> getSt() {
+        List <String> l =  new ArrayList<>();
+        l.add("dd1");
+        l.add("dd2");
+        l.add("dd3");
+        l.add("dd4");
+        return l;
+    }
+
+
+    public static void main() throws Exception {
+        try (ConnectToDB demo = new ConnectToDB()) {
+            demo.getShops();
+        }
+    }
+
+    @Override
+    public void close() throws Exception {
+
+    }
 }
+
