91c37e4c6d28ac83bde0602de931eed33e93c623
katandser <katandser96@mail.ru>
2019-06-03 13:50:46 +0700

    create class shop

--- src/main/java/entitys/Shop.java
+++ src/main/java/entitys/Shop.java
@@ -1,4 +1,25 @@
 package entitys;
 
 public class Shop {
+    private String id;
+    private String name;
+    private String adress;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public Shop(String id, String name, String adress) {
+        this.id = id;
+        this.name = name;
+        this.adress = adress;
+    }
 }
