5e9c145009ad3afd52fef139e0e28fe251baa61e
katandser <katandser96@mail.ru>
2019-06-03 13:47:48 +0700

    init class shop

--- src/main/java/jdbc/ConnectToDB.java
+++ src/main/java/jdbc/ConnectToDB.java
@@ -6,8 +6,7 @@ import java.util.List;
 
 public class ConnectToDB implements AutoCloseable {
     private static final String DB_URL = "jdbc:clickhouse://localhost:8123/db_test";
-    private final Connection conn;
-
+    private static Connection conn;
     /**
      * Creates new instance
      * @throws SQLException in case of connection issue
@@ -15,19 +14,21 @@ public class ConnectToDB implements AutoCloseable {
     public ConnectToDB() throws SQLException {
         conn = DriverManager.getConnection(DB_URL,"","1");
     }
-
     /**
      * Queries db to get most popular flight route for ths given year
      * @throws SQLException in case of query issue
      */
-    public void getShops() throws SQLException {
-        String query = "select * from db_test.events where site_id = 1";
+    public static List<String> getShops() throws SQLException {
+        conn = DriverManager.getConnection(DB_URL,"","1");
+        String query = "select * from db_test.shop";
         Statement statement = conn.createStatement();
         ResultSet rs = statement.executeQuery(query);
         ResultSetMetaData rsMetadata = rs.getMetaData();
+        List <String> listShops =  new ArrayList<>();
         while (rs.next()) {
             for (int i = 1; i <= rsMetadata.getColumnCount(); i++) {
                 System.out.print(rs.getString(i) + " ");
+                listShops.add(rs.getString(i));
             }
             System.out.println();
         }
@@ -35,10 +36,6 @@ public class ConnectToDB implements AutoCloseable {
 
     public static List<String> getSt() {
         List <String> l =  new ArrayList<>();
-        l.add("dd1");
-        l.add("dd2");
-        l.add("dd3");
-        l.add("dd4");
         return l;
     }
 
